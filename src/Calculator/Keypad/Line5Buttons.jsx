163cb3f4db7a2a8b5dec56d4dc4b293b3f57f53d
Luis Angel <langelgirona@gmail.com>
2026-05-05 18:30:25 -0600

    feat: add Zero button image to enhance visual representation in Line5Buttons

--- src/Calculator/Keypad/Line5Buttons.jsx
+++ src/Calculator/Keypad/Line5Buttons.jsx
@@ -1,9 +1,10 @@
 import Button from './Button'
+import image0 from './Images/Zero.png'
 
 export default function Line5Buttons({ onNumber, onEquals }) {
   return (
     <div className="button-row row-5">
-      <Button label="0" className="btn" onClick={() => onNumber('0')} />
+      <Button label="0" imageSrc={image0} className="btn" onClick={() => onNumber('0')} />
       <Button label="." className="btn" onClick={() => {}} />
       <Button label="=" className="btn-equals" onClick={onEquals} />
     </div>
