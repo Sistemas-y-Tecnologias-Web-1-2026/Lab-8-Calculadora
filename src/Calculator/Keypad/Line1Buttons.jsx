70ee0aa8c83a2da617f1f619f3286613cca72e94
Luis Angel <langelgirona@gmail.com>
2026-05-05 19:36:15 -0600

    feat: update button classes for improved styling and functionality in Line1Buttons and Line5Buttons

--- src/Calculator/Keypad/Line1Buttons.jsx
+++ src/Calculator/Keypad/Line1Buttons.jsx
@@ -4,8 +4,8 @@ export default function Line1Buttons({ onOp, onClear, changeSign }) {
   return (
     <div className="button-row">
       <Button label="C" className="btn-special" onClick={onClear} />
-      <Button label="+/-" className="btn" onClick={changeSign} />
-      <Button label="%" className="btn-percent" onClick={() => onOp('%')} />
+      <Button label="+/-" className="btn-operation" onClick={changeSign} />
+      <Button label="%" className="btn-operation" onClick={() => onOp('%')} />
       <Button label="/" className="btn-operation" onClick={() => onOp('/')} />
     </div> 
   )
--- src/Calculator/Keypad/Line5Buttons.jsx
+++ src/Calculator/Keypad/Line5Buttons.jsx
@@ -5,7 +5,7 @@ export default function Line5Buttons({ onNumber, onEquals }) {
   return (
     <div className="button-row row-5">
       <Button label="0" imageSrc={image0} className="btn" onClick={() => onNumber('0')} />
-      <Button label="." className="btn" onClick={() => {}} />
+      <Button label="." className="btn-operation" onClick={() => {}} />
       <Button label="=" className="btn-equals" onClick={onEquals} />
     </div>
   )
