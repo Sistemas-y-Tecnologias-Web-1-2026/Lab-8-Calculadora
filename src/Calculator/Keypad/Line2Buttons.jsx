a0e709dd4a83fc23cc492706e416401c49fea077
Luis Angel <langelgirona@gmail.com>
2026-05-05 18:20:17 -0600

    feat: add new button images and update button components to use images for better visual representation

--- src/Calculator/Keypad/Line2Buttons.jsx
+++ src/Calculator/Keypad/Line2Buttons.jsx
@@ -1,11 +1,14 @@
 import Button from './Button'
+import esfera7 from './Images/Esfera7.png'
+import image8 from './Images/Liquir.webp'
+import image9 from './Images/Bergamo.webp'
 
 export default function Line2Buttons({ onNumber, onOp }) {
   return (
     <div className="button-row">
-      <Button label="7" className="btn" onClick={() => onNumber('7')} />
-      <Button label="8" className="btn" onClick={() => onNumber('8')} />
-      <Button label="9" className="btn" onClick={() => onNumber('9')} />
+      <Button label="7" imageSrc={esfera7} className="btn" onClick={() => onNumber('7')} />
+      <Button label="8" imageSrc={image8} className="btn" onClick={() => onNumber('8')} />
+      <Button label="9" imageSrc={image9} className="btn" onClick={() => onNumber('9')} />
       <Button label="*" className="btn-operation" onClick={() => onOp('*')} />
     </div>
   )
--- src/Calculator/Keypad/Line4Buttons.jsx
+++ src/Calculator/Keypad/Line4Buttons.jsx
@@ -1,11 +1,14 @@
 import Button from "./Button"
+import esfera1 from "./Images/Esfera1.png"
+import esfera2 from "./Images/Esfera2.png"
+import esfera3 from "./Images/Esfera3.png"
 
 export default function Line4Buttons({ onNumber, onOp }) {
   return (
     <div className="button-row">
-      <Button label="1" className="btn" onClick={() => onNumber('1')} />
-      <Button label="2" className="btn" onClick={() => onNumber('2')} />
-      <Button label="3" className="btn" onClick={() => onNumber('3')} />
+      <Button label="1" imageSrc={esfera1} className="btn" onClick={() => onNumber('1')} />
+      <Button label="2" imageSrc={esfera2} className="btn" onClick={() => onNumber('2')} />
+      <Button label="3" imageSrc={esfera3} className="btn" onClick={() => onNumber('3')} />
       <Button label="+" className="btn-operation" onClick={() => onOp('+')} />
     </div>
   )
